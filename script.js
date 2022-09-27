8b6f7bdda2bb61ba65411251b9d0314e82602d96
Alexander Rodzyanko <alexander.rodzyanko@gmail.com>
2022-09-27 15:32:42 +0900

    Add function to script that stores new book objects and pushes to array

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
+// declare empty array for library
 let myLibrary = [];
 
-// constructor
+// object constructor
 function Book() {
    this.title = title;
    this.author = author;
    this.pages = pages;
    this.read = read;
-   this.info = function() {
-      return (title + author + ', ' + pages + read)
-   }
+   // this.info = function() {
+   //    return (title + author + ', ' + pages + read)
+   // }
 }
 
 const theHobbit = new Book('The Hobbit', 'by J.R.R. Tolkien', '297 pages', 'not read')
 console.log(theHobbit.info());
 
-function addBookToLibrary() {
-   // do stuff here
+
+// adds new books to array
+function addBookToLibrary(title, author, pages, read) {
+   let book = new Book(title, author, pages, read);
+   myLibrary.push(book);
 }
