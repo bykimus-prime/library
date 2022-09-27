ea65eec3de4c1c8bbecec8ac036771139107826e
Alexander Rodzyanko <alexander.rodzyanko@gmail.com>
2022-09-27 16:00:59 +0900

    Add loop to create cards with book info and display to page

--- script.js
+++ script.js
@@ -12,12 +12,30 @@ function Book() {
    // }
 }
 
-const theHobbit = new Book('The Hobbit', 'by J.R.R. Tolkien', '297 pages', 'not read')
-console.log(theHobbit.info());
+// const theHobbit = new Book('The Hobbit', 'by J.R.R. Tolkien', '297 pages', 'not read')
+// console.log(theHobbit.info());
 
 
 // adds new books to array
 function addBookToLibrary(title, author, pages, read) {
    let book = new Book(title, author, pages, read);
    myLibrary.push(book);
+}
+
+// displays library array to cards
+function displayBooks() {
+   const books = document.querySelector('.books');
+
+   // loop over library array and display to cards
+   myLibrary.forEach(myLibrary => {
+      const card = document.createElement('div');
+      card.classList.add('card')
+      books.appendChild(card);
+      for (let key in myLibrary) {
+         console.log(`${key}: ${myLibrary[key]}`)
+         const para = document.createElement('p');
+         para.textContent = (`${key}: ${myLibrary[key]}`);
+         card.appendChild(para);
+      }
+   })
 }
